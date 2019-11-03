da163146c008242e3cf70b1e1148c3cb6ac436b4
Ruipeng Wang <>
2019-11-03 17:22:02 +0800

    feat: add cucumber reporting dependency to make test report friendly

--- src/test/java/examples/ExamplesTest.java
+++ src/test/java/examples/ExamplesTest.java
@@ -1,6 +1,16 @@
 package examples;
 
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 class ExamplesTest {
 
@@ -8,7 +18,17 @@ class ExamplesTest {
     // see https://github.com/intuit/karate#naming-conventions
     @Karate.Test
     Karate testAll() {
+        Results results = Runner.parallel(getClass(), 1);
+        generateReport(results.getReportDir());
         return new Karate().relativeTo(getClass());
     }
 
+    static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "My Cases");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 }
